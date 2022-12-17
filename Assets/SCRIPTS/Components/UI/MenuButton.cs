4c379616dde5c98261432c03782a460289911137
David Antonio Vélez Rebollo <david_velezreb@hotmail.com>
2022-12-17 22:50:39 +0100

    Navegación por menús y selección de objetos
    Se ha añadido la navegación mediante mando y teclado por el menú de pausa del juego. Además, se ha solucionado el bug que se daba cuando el personaje se acercaba demasiado a un objeto recogible

--- Assets/SCRIPTS/Components/UI/MenuButton.cs
+++ Assets/SCRIPTS/Components/UI/MenuButton.cs
@@ -3,12 +3,21 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace QuantumWeavers.Components.UI {
-    public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler {
         public void OnPointerEnter(PointerEventData eventData) {
             transform.DOScale(new Vector3(1.3f, 1.3f, 0f), 1f).SetEase(Ease.OutFlash);
         }
 
         public void OnPointerExit(PointerEventData eventData) {
+            transform.DOPause();
+            transform.localScale = Vector3.one;
+        }
+
+        public void OnSelect(BaseEventData eventData) {
+            transform.DOScale(new Vector3(1.3f, 1.3f, 0f), 1f).SetEase(Ease.OutFlash);
+        }
+
+        public void OnDeselect(BaseEventData eventData) {
             transform.DOPause();
             transform.localScale = Vector3.one;
         }
