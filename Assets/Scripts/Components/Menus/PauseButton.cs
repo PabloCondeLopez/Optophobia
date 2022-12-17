0202879b56df44409dc3eb9ae9193d6ee2e2053e
David Antonio Vélez Rebollo <david_velezreb@hotmail.com>
2022-12-17 23:52:11 +0100

    Version final del control del menu mediante el mando

--- Assets/Scripts/Components/Menus/PauseButton.cs
+++ Assets/Scripts/Components/Menus/PauseButton.cs
@@ -4,34 +4,44 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class PauseButton : MonoBehaviour, ISelectHandler, IDeselectHandler
-{
-    public bool isActive { set; get; }
-    [SerializeField] private GameObject panel;
-    private Image image;
-    [SerializeField] private Sprite _selected;
-    [SerializeField] private Sprite _nonSelected;
-    [SerializeField] private Sprite _selectedScribbles;
-    [SerializeField] private Sprite _nonSelectedScribbles;
+public class PauseButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler {
+    
+    public bool IsActive;
+    [SerializeField] private GameObject Panel;
+    private Image _image;
+    [SerializeField] private Sprite Selected;
+    [SerializeField] private Sprite NonSelected;
+    [SerializeField] private Sprite SelectedScribbles;
+    [SerializeField] private Sprite NonSelectedScribbles;
 
     private void Start()
     {
-        image = GetComponent<Image>();
-        image.sprite = _nonSelected;
+        _image = GetComponent<Image>();
+        _image.sprite = IsActive ? NonSelectedScribbles : NonSelected;
+    }
+
+    public void OnPointerEnter(PointerEventData data) {
+        _image.sprite = IsActive ? SelectedScribbles : Selected;
+    }
+    
+    public void OnPointerExit(PointerEventData data) {
+        _image.sprite = IsActive ? NonSelectedScribbles : NonSelected;
     }
 
     public void OnSelect(BaseEventData eventData) 
     {
-        image.sprite = isActive ? _selectedScribbles : _nonSelectedScribbles;
+        _image.sprite = IsActive ? SelectedScribbles : Selected;
     }
+    
     public void OnDeselect(BaseEventData eventData)
     {
-        image.sprite = isActive ? _nonSelectedScribbles : _nonSelected;
+        _image.sprite = IsActive ? NonSelectedScribbles : NonSelected;
     }
 
     public void ChangeOfState(bool active)
     {
-        panel.SetActive(active);
-        isActive = active;
+        Panel.SetActive(active);
+        IsActive = active;
+        _image.sprite = IsActive ? SelectedScribbles : NonSelected;
     }
 }
