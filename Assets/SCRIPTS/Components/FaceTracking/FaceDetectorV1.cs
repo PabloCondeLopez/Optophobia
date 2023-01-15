61036929c31ccbb667a9f07329bb3c331558f811
David Antonio Vélez Rebollo <david_velezreb@hotmail.com>
2023-01-15 02:42:24 +0100

    No tengo ni idea de qué va mal

--- Assets/SCRIPTS/Components/FaceTracking/FaceDetectorV1.cs
+++ Assets/SCRIPTS/Components/FaceTracking/FaceDetectorV1.cs
@@ -1,109 +1,79 @@
-using OpenCvSharp.Demo;
 using UnityEngine;
-using UnityEngine.UI;
+using OpenCVForUnity.CoreModule;
+using OpenCVForUnity.ImgprocModule;
+using OpenCVForUnity.ObjdetectModule;
+using Rect = OpenCVForUnity.CoreModule.Rect;
 
 namespace QuantumWeavers.Components.FaceTracking {
 	public class FaceDetectorV1 : MonoBehaviour {
-		
-		[SerializeField] private GameObject Surface;
+		private bool _firstClose = false;
 
-		[SerializeField] private TextAsset Faces;
-		[SerializeField] private TextAsset Eyes;
+		[SerializeField] private bool DebugMode = false;
+		[SerializeField] private MeshRenderer Material;
 
-		private WebCamDevice? _webCamDevice;
+		private CascadeClassifier _faceCascade;
+		private CascadeClassifier _eyeCascade;
 		private WebCamTexture _webCamTexture;
-		private Texture2D _renderedTexture;
-		private bool _forceFrontalCamera;
-		private OpenCvSharp.Unity.TextureConversionParams _textureParameters;
-		private FaceProcessorLive<WebCamTexture> _processor;
+		private Mat _rgbaMat;
+		private Mat _grayMat;
 
-		private string DeviceName {
-			get => _webCamDevice?.name;
+		private void Start() {
+			string facePath = Application.dataPath + "/SCRIPTS/Components/FaceTracking/haarcascade_frontalface_default.xml";
+			string eyePath = Application.dataPath + "/SCRIPTS/Components/FaceTracking/haarcascade_eye.xml";
 
-			set {
-				if (value == DeviceName) return;
-				
-				if (_webCamTexture && _webCamTexture.isPlaying) _webCamTexture.Stop();
+			_faceCascade = new CascadeClassifier(facePath);
+			_eyeCascade = new CascadeClassifier(eyePath);
 
-				int cameraIndex = -1;
-				
-				for(int i = 0; i < WebCamTexture.devices.Length; i++)
-					if (WebCamTexture.devices[i].name == value)
-						cameraIndex = i;
+			_webCamTexture = new WebCamTexture();
+			_webCamTexture.Play();
+			Material.material.mainTexture = _webCamTexture;
 
-				if (cameraIndex == -1) return;
-				
-				_webCamDevice = WebCamTexture.devices[cameraIndex];
-				_webCamTexture = new WebCamTexture(_webCamDevice.Value.name);
-
-				UpdateTextureConversionParameters();
-				
-				_webCamTexture.Play();
-			}
-		}
-
-		private void Awake() {
-			if (WebCamTexture.devices.Length > 0) DeviceName = WebCamTexture.devices[0].name;
-			_forceFrontalCamera = true;
-
-			_processor = new FaceProcessorLive<WebCamTexture>();
-			_processor.Initialize(Faces.text, Eyes.text);
-
-			_processor.DataStabilizer.Enabled = true;
-			_processor.DataStabilizer.Threshold = 2.0;
-			_processor.DataStabilizer.SamplesCount = 2;
-
-			_processor.Performance.Downscale = 256;
-			_processor.Performance.SkipRate = 0;
-
+			_rgbaMat = new Mat(_webCamTexture.height, _webCamTexture.width, CvType.CV_8UC4);
+			_grayMat = new Mat(_webCamTexture.height, _webCamTexture.width, CvType.CV_8UC1);
 		}
 
 		private void Update() {
-			if (_webCamTexture && _webCamTexture.didUpdateThisFrame) {
-				UpdateTextureConversionParameters();
+			Imgproc.cvtColor(_rgbaMat, _grayMat, Imgproc.COLOR_RGBA2GRAY);
+			Imgproc.bilateralFilter(_grayMat, _grayMat, 5, 1, 1);
+
+			MatOfRect faces = new MatOfRect();
+			_faceCascade.detectMultiScale(_grayMat, faces, 1.3, 5, 0, new Size(50, 50));
+
+			if (faces.toArray().Length <= 0) {
+				Imgproc.putText(_rgbaMat, "No face detected", new Point(100, 100),
+					Imgproc.FONT_HERSHEY_PLAIN, 3, new Scalar(0, 255, 0), 2);
+				Debug.Log("No face detected");
+				return;
+			}
+
+			foreach (Rect face in faces.toArray()) {
+				Mat roiFace = _grayMat.submat(face);
+				Mat roiFaceColor = _rgbaMat.submat(face);
 				
-				if(ProcessTexture(_webCamTexture, ref _renderedTexture))
-					RenderFrame();
+				MatOfRect eyes = new MatOfRect();
+				_eyeCascade.detectMultiScale(roiFace, eyes, 1.3, 5, 0, new Size(10, 10));
+
+				if (eyes.toArray().Length >= 2) {
+					if(DebugMode)
+						Imgproc.putText(_rgbaMat, "Eyes Open!", new Point(face.x, face.y - 20), 
+							Imgproc.FONT_HERSHEY_PLAIN, 2, new Scalar(255, 255, 255), 2);
+
+					foreach (Rect eye in eyes.toArray()) 
+						Imgproc.rectangle(roiFaceColor, eye.tl(), eye.br(), new Scalar(255, 0, 0), 2);
+
+					_firstClose = false;
+				}
+				else {
+					if(DebugMode)
+						Imgproc.putText(_rgbaMat, "Eyes Closed", new Point(face.x, face.y - 20), 
+							Imgproc.FONT_HERSHEY_PLAIN, 3, new Scalar(0, 0, 255), 2);
+
+					if (_firstClose) {
+						Debug.Log("Eyes closed, you can advance");
+						_firstClose = true;
+					}
+				}
 			}
 		}
-
-		private void OnDestroy() {
-			if (_webCamTexture != null) {
-				if(_webCamTexture.isPlaying) _webCamTexture.Stop();
-				_webCamTexture = null;
-			}
-
-			_webCamDevice = null;
-		}
-
-		private void UpdateTextureConversionParameters() {
-			OpenCvSharp.Unity.TextureConversionParams parameters = new() {
-				FlipHorizontally = _forceFrontalCamera || _webCamDevice.Value.isFrontFacing
-			};
-
-			if (_webCamTexture.videoRotationAngle != 0)
-				parameters.RotationAngle = _webCamTexture.videoRotationAngle;
-
-			_textureParameters = parameters;
-		}
-
-		private void RenderFrame() {
-			if (!_renderedTexture) return;
-
-			Surface.GetComponent<RawImage>().texture = _renderedTexture;
-			Surface.GetComponent<RectTransform>().sizeDelta =
-				new Vector2(_renderedTexture.width, _renderedTexture.height);
-		}
-
-		private bool ProcessTexture(WebCamTexture input, ref Texture2D output) {
-			_processor.ProcessTexture(input, _textureParameters);
-			
-			_processor.MarkDetected();
-
-			output = OpenCvSharp.Unity.MatToTexture(_processor.Image, output);
-
-			return true;
-		}
-		
 	}
 }
